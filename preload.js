d374b0bc6ab3acaa070bbbc432ba53aa8b2049b5
codeandcurves <codeandcurves.ai@gmail.com>
2026-09-15 18:48:57 +0530

    feat: expose local Spark inbox to renderer

--- preload.js
+++ preload.js
@@ -1,7 +1,11 @@
 const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('sparkDesktop',{
- close:()=>ipcRenderer.send('close-popup'), openUrl:u=>ipcRenderer.send('open-url',u),
- hover:on=>ipcRenderer.send('popup-hover',!!on), showAnother:()=>ipcRenderer.send('show-another'),
- getQuote:()=>ipcRenderer.invoke('get-internet-quote'), getPrefs:()=>ipcRenderer.invoke('get-prefs'),
- savePrefs:p=>ipcRenderer.invoke('save-prefs',p)
+ close:()=>ipcRenderer.send('close-popup'),
+ hover:on=>ipcRenderer.send('popup-hover',!!on),
+ showAnother:()=>ipcRenderer.send('show-another'),
+ getInbox:()=>ipcRenderer.invoke('get-inbox'),
+ getPrefs:()=>ipcRenderer.invoke('get-prefs'),
+ savePrefs:p=>ipcRenderer.invoke('save-prefs',p),
+ onRefresh:fn=>ipcRenderer.on('refresh-spark',()=>fn()),
+ onDisplay:fn=>ipcRenderer.on('display-spark',(_e,item)=>fn(item))
 });
