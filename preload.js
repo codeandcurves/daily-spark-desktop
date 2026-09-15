2f24379a17d476c086ab94f3fb0223a0498842f3
codeandcurves <codeandcurves.ai@gmail.com>
2026-09-15 20:03:35 +0530

    feat: expose Daily Spark 2 actions

--- preload.js
+++ preload.js
@@ -2,10 +2,11 @@ const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('sparkDesktop',{
  close:()=>ipcRenderer.send('close-popup'),
  hover:on=>ipcRenderer.send('popup-hover',!!on),
- showAnother:()=>ipcRenderer.send('show-another'),
- getInbox:()=>ipcRenderer.invoke('get-inbox'),
- getPrefs:()=>ipcRenderer.invoke('get-prefs'),
- savePrefs:p=>ipcRenderer.invoke('save-prefs',p),
- onRefresh:fn=>ipcRenderer.on('refresh-spark',()=>fn()),
+ another:()=>ipcRenderer.invoke('next-spark'),
+ later:()=>ipcRenderer.invoke('later-spark'),
+ done:()=>ipcRenderer.invoke('done-spark'),
+ importHistory:()=>ipcRenderer.invoke('import-history'),
+ captureClipboard:()=>ipcRenderer.invoke('capture-clipboard'),
+ stats:()=>ipcRenderer.invoke('stats'),
  onDisplay:fn=>ipcRenderer.on('display-spark',(_e,item)=>fn(item))
 });
